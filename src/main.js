253ec972f09a0ef3027c71d9083d5eadf60edf6f
TarasLys <opros0609@gmail.com>
2023-10-30 21:28:53 +0200

    second

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ function onFormInput(e) {
   e.preventDefault();
   const formData = new FormData(e.target);
   const data = Object.fromEntries(formData);
+  console.log(data)
   e.target.reset();
   data.createdAt = Date.now();
   serviceWriteData(data)
